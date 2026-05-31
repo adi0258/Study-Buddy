ca88de9b791c856754369f29d9a18fd01221d7c6
adi0258 <adita3@mta.ac.il>
2026-05-31 16:22:01 +0300

    ux/ui 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // יש לשנות את היבוא מ-react-dom ל-react-dom/client
-import './styles/LoginForm.css'; // ייבוא קובץ ה-CSS
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // יצירת root חדש
